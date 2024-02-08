8a7128a7c69c685509a770e7bd7d7e9ca98a0e74
wataru-51 <kw22304025@ga.ttc.ac.jp>
2024-02-08 11:57:25 +0900

    ｆ

---
+++ src/main/java/jp/ac/ttc/webapp/HelloServlet.java
@@ -0,0 +1,19 @@
+package jp.ac.ttc.webapp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();
+		out.write("<h1>hello, world</h1>");
+	}
+}
